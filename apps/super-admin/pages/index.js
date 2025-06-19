4ceccdc5b4ba1d85779be6bbdef2629b76fb1d2c
rwbgoachic <rwbchicago@gmail.com>
2025-06-19 06:28:21 -0500

    Delete apps/super-admin directory

--- apps/super-admin/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:'2rem', fontFamily:'sans-serif'}}>
-      <h1>Welcome to Super Admin Ui</h1>
-      <p>If you can read this, the Next.js build worked!</p>
-    </main>
-  );
-}
